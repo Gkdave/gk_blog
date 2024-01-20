bd420b17fccd5ac3fc4b0950632d3fa3a120f3d3
gajendra <davegk59@gmil.com>
2024-01-20 12:05:20 +0530

    added index, models,register

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User 
+from datetime import datetime
 
 # Create your models here.
+class Blog(models.Model):
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    desc = models.TextField(null=False)
+    date=datetime.today()
+    def __str__(self):
+        return self.title
+    
--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 from datetime import datetime
+from .models import Blog 
 
 # Create your views here.
 def index(request):
-    date=datetime.today()
-    context = {
-      'name': "gajendra",
-      'date': date
-        
-    }
     
-    return render(request,'home.html',context)
+    blog = Blog.objects.all()
+    return render(request,'home.html',{'blog':blog})
+  
+def user_register(request):
+  return render(request,'register.html')
